3f5225e9c9dec75debfb117e44f58e0ff3d33fe8
kneradovsky <kneradovsky@gmail.com>
2021-07-01 15:22:52 +0300

    added video

--- src/app/qrverify/qrverify.component.ts
+++ src/app/qrverify/qrverify.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-qrverify',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QrverifyComponent implements OnInit {
 
-  constructor() { }
+  qrvideo :any;
+  played = false;
+  track0: MediaStreamTrack | undefined;
+  stream: MediaStream | undefined
 
-  ngOnInit(): void {
+  constructor() { 
+
+  }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.qrvideo=document.querySelector("#qrcode")
+      this.stream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: {
+            ideal: "enviromnent"
+          }
+        }
+      })
+      this.track0 = this.stream.getVideoTracks()[0]
+      console.debug(`get video from ${this.track0.label}`)
+      this.qrvideo.srcObject = this.stream
+      this.qrvideo.play()
+      this.played=true
+    } catch(e) {
+      console.debug(e)
+    }
+  }
+  
+  onToggle() {
+    if(this.played) {
+      this.track0?.stop()
+      this.qrvideo.display="none";
+    }
+    else {
+      this.ngOnInit()
+      this.qrvideo.display="box";
+    }
+    this.played=!this.played
   }
 
 }
